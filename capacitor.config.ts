9513caf0e3034c69c44871bf81117b9854c9a0e4
JoseDiasp <113144397+JoseDiasp@users.noreply.github.com>
2026-02-18 20:42:09 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'manejo',
+  webDir: 'www'
+};
+
+export default config;
